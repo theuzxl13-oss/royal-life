25d3196f1265ffe3dbc09d089cc9866c7e0efd28
matheus marcelino <theuzxl13@gmail.com>
2026-08-20 16:29:50 -0300

    Ativa DEBUG True temporario

--- setup/settings.py
+++ setup/settings.py
@@ -10,8 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # CHAVE DE SEGURANÇA
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-sua-chave-secreta-aqui')
 
-# DEBUG: Desativa em produção se houver variável RENDER
-DEBUG = 'RENDER' not in os.environ
+# DEBUG ATIVADO TEMPORARIAMENTE PARA IDENTIFICAR O ERRO
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
