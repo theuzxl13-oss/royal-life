84e3b7007c616121b4949d25c473b097408ad332
matheus marcelino <theuzxl13@gmail.com>
2026-08-20 16:29:49 -0300

    Garante uso do modulo setup no settings

--- setup/urls.py
+++ setup/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import home, colecao
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('colecao/', colecao, name='colecao'),
+    path('', include('core.urls')), # ou as rotas do seu app core
 ]
 
-# Libera o acesso às imagens cadastradas no admin
-if settings.DEBUG or True:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
