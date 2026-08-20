e84f606c92f275cc6b942bb10be3e708c603b43c
matheus marcelino <theuzxl13@gmail.com>
2026-08-20 16:29:54 -0300

    SEGURANCA: remove rota publica que resetava senha do admin sem autenticacao

--- setup/urls.py
+++ setup/urls.py
@@ -3,21 +3,10 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.contrib.auth.models import User
-from django.http import HttpResponse
 from core import views
-
-def reset_admin(request):
-    user, created = User.objects.get_or_create(username='admin')
-    user.set_password('Admin123456')
-    user.is_superuser = True
-    user.is_staff = True
-    user.save()
-    return HttpResponse("✅ Usuário 'admin' atualizado com sucesso! A nova senha é: Admin123456")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reset-admin/', reset_admin),
     path('', views.home, name='home'),
     path('colecao/', views.colecao, name='colecao'),
     
