07e31988474d655f2d7455c30a59672c94c99867
matheus marcelino <theuzxl13@gmail.com>
2026-08-20 16:29:49 -0300

    Permite servir arquivos de media no Render

--- setup/urls.py
+++ setup/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from core import views
 
 def reset_admin(request):
     user, created = User.objects.get_or_create(username='admin')
-    user.set_password('Admin123456') # Definindo a senha temporária
+    user.set_password('Admin123456')
     user.is_superuser = True
     user.is_staff = True
     user.save()
@@ -16,9 +17,12 @@ def reset_admin(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reset-admin/', reset_admin), # Rota de reset
+    path('reset-admin/', reset_admin),
     path('', views.home, name='home'),
     path('colecao/', views.colecao, name='colecao'),
+    
+    # Permite exibir imagens da pasta media no Render
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
