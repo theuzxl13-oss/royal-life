28d9448d9c9c63051d75e9aa258e4c0e887f88d9
matheus marcelino <theuzxl13@gmail.com>
2026-08-20 16:29:50 -0300

    Adiciona cloudinary.config() explicito

--- setup/settings.py
+++ setup/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import dj_database_url
+import cloudinary
 
 # Diretórios
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,12 +91,20 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# CONFIGURAÇÃO DO CLOUDINARY (Armazenamento de mídia)
+# CONFIGURAÇÃO DO CLOUDINARY
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
+
+# Inicialização do SDK do Cloudinary
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True
+)
 
 # Suporte de armazenamento para Django
 STORAGES = {
