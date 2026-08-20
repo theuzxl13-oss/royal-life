d225e1dee9e721b13ecfc73d268e7ab7fc1f455a
matheus marcelino <theuzxl13@gmail.com>
2026-08-20 16:29:46 -0300

    Cria rota e pagina da colecao com filtros de genero e marca

--- core/views.py
+++ core/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 from .models import Perfume
 
 def home(request):
+    return render(request, 'core/home.html')
+
+def colecao(request):
+    genero = request.GET.get('genero')
+    marca = request.GET.get('marca')
+    
     perfumes = Perfume.objects.all()
-    return render(request, 'core/home.html', {'perfumes': perfumes})
+    
+    if genero:
+        perfumes = perfumes.filter(genero=genero)
+    if marca:
+        perfumes = perfumes.filter(marca=marca)
+        
+    context = {
+        'perfumes': perfumes,
+        'genero_selecionado': genero,
+        'marca_selecionada': marca,
+    }
+    return render(request, 'core/colecao.html', context)
--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from core.views import home
+from core.views import home, colecao
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('colecao/', colecao, name='colecao'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
