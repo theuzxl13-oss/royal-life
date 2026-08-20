03f3e184c585e897de0f1c4fb3050fe0504bee6a
matheus marcelino <theuzxl13@gmail.com>
2026-08-20 16:29:49 -0300

    Ajusta WSGI e URLCONF para a pasta setup

--- core/views.py
+++ core/views.py
@@ -1,37 +1,36 @@
 from django.shortcuts import render
-from .models import Perfume
-
-def home(request):
-    # Pega os 4 últimos perfumes cadastrados para exibir em Destaques/Lançamentos
-    lancamentos = Perfume.objects.all().order_by('-criado_em')[:4]
-    return render(request, 'core/home.html', {'lancamentos': lancamentos})
-
-def colecao(request):
-    genero = request.GET.get('genero')
-    marca = request.GET.get('marca')
-    
-    perfumes = Perfume.objects.all()
-    
-    if genero:
-        perfumes = perfumes.filter(genero=genero)
-    if marca:
-        perfumes = perfumes.filter(marca=marca)
-        
-    context = {
-        'perfumes': perfumes,
-        'genero_selecionado': genero,
-        'marca_selecionada': marca,
-    }
-    return render(request, 'core/colecao.html', context)
-from django.shortcuts import render
 from .models import Perfume, Campanha
+from django.db.utils import ProgrammingError, OperationalError
 
 def home(request):
-    lancamentos = Perfume.objects.all()[:4] # ou seu filtro atual
-    campanha_ativa = Campanha.objects.filter(ativa=True).first() # Pega a primeira campanha ativa
+    lancamentos = Perfume.objects.all()[:4]
+    
+    # Busca a campanha. Se a tabela ainda não existir no banco, ignora para não dar erro 500
+    try:
+        campanha_ativa = Campanha.objects.filter(ativa=True).first()
+    except (ProgrammingError, OperationalError):
+        campanha_ativa = None
 
     context = {
         'lancamentos': lancamentos,
         'campanha_ativa': campanha_ativa,
     }
     return render(request, 'core/home.html', context)
+
+def colecao(request):
+    perfumes = Perfume.objects.all()
+    
+    genero = request.GET.get('genero')
+    marca = request.GET.get('marca')
+
+    if genero:
+        perfumes = perfumes.filter(genero=genero)
+    if marca:
+        perfumes = perfumes.filter(marca=marca)
+
+    context = {
+        'perfumes': perfumes,
+        'genero_selecionado': genero,
+        'marca_selecionada': marca,
+    }
+    return render(request, 'core/colecao.html', context)
