c3eb22f9cd9fb512f7981d3fe31d3a4c763a7982
matheus marcelino <theuzxl13@gmail.com>
2026-08-20 16:29:47 -0300

    Atualiza layout da home com logo, lancamentos e secao institucional

--- core/views.py
+++ core/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import Perfume
 
 def home(request):
-    return render(request, 'core/home.html')
+    # Pega os 4 últimos perfumes cadastrados para exibir em Destaques/Lançamentos
+    lancamentos = Perfume.objects.all().order_by('-criado_em')[:4]
+    return render(request, 'core/home.html', {'lancamentos': lancamentos})
 
 def colecao(request):
     genero = request.GET.get('genero')
@@ -20,5 +22,4 @@ def colecao(request):
         'genero_selecionado': genero,
         'marca_selecionada': marca,
     }
-    
     return render(request, 'core/colecao.html', context)
